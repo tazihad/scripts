6c27315cf6dfb2a84e3d360131ae224b9ac39aa7
Zihad <zihad.ta@gmail.com>
2024-07-25 21:21:35 +0600

    Add files via upload

---
+++ flatpak-apps.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+# This script is licensed under the GNU General Public License v3.
+# See LICENSE file for details.
+
+# Author: @tazihad
+
+# Description: Favourite flatpak apps
+
+flatpak install -y flathub org.kde.okular \
+                      org.kde.gwenview \
+                      org.kde.audiotube \
+                      org.kde.elisa \
+                      org.kde.kclock \
+                      org.kde.krita \
+                      org.kde.kcalc \
+                      org.kde.plasmatube \
+                      org.kde.kfind \
+                      org.kde.marknote \
+                      org.kde.koko \
+                      org.kde.kamoso \
+                      org.kde.kget \
+                      org.kde.yakuake \
+                      org.kde.neochat \
+                      org.kde.kolourpaint \
+                      org.kde.kcolorchooser \
+                      org.kde.kalk \
+                      org.kde.falkon \
+                      org.kde.kdf \
+                      org.kde.kompare \
+                      org.kde.kdiff3 \
+                      org.kde.kbreakout \
+                      org.kde.kasts \
+                      org.kde.kgeography \
+                      org.kde.keysmith \
+                      org.kde.krdc \
+                      org.kde.haruna \
+                      org.kde.alligator \
+                      org.kde.konquest \
+                      org.kde.knavalbattle \
+                      org.kde.knights \
+                      org.kde.kmines \
+                      org.kde.kpat \
+                      org.kde.kmahjongg \
+                      com.usebottles.bottles \
+                      com.github.tchx84.Flatseal \
+                      io.github.peazip.PeaZip \
+                      io.github.flattool.Warehouse \
+                      it.mijorus.gearlever \
+                      org.keepassxc.KeePassXC \
+                      org.localsend.localsend_app \
+                      io.github.dvlv.boxbuddyrs \
+                      io.github.giantpinkrobots.flatsweep \
+                      com.github.dail8859.NotepadNext \
+                      io.emeric.toolblex \
+                      io.github.congard.qnvsm \
+                      io.gitlab.adhami3310.Converter \
+                      com.github.ADBeveridge.Raider \
+                      net.sapples.LiveCaptions \
+                      com.beavernotes.beavernotes \
+                      com.belmoussaoui.Obfuscate \
+                      com.endlessm.photos \
+                      org.qownnotes.QOwnNotes \
+                      io.exodus.Exodus \
+                      com.bktus.gpgfrontend \
+                      io.gitlab.persiangolf.voicegen \
+                      io.otsaloma.nfoview \
+                      md.obsidian.Obsidian \
+                      org.libreoffice.LibreOffice \
+                      org.mozilla.Thunderbird \
+                      com.github.marhkb.Pods \
+                      org.videolan.VLC \
+                      com.obsproject.Studio \
+                      fr.handbrake.ghb \
+                      com.github.wwmm.easyeffects \
+                      io.mpv.Mpv \
+                      org.strawberrymusicplayer.strawberry \
+                      org.nickvision.tubeconverter \
+                      com.dec05eba.gpu_screen_recorder \
+                      info.smplayer.SMPlayer \
+                      no.mifi.losslesscut \
+                      com.ozmartians.VidCutter \
+                      net.mediaarea.MediaInfo \
+                      org.gimp.GIMP \
+                      org.upscayl.Upscayl \
+                      com.github.vikdevelop.photopea_app \
+                      com.github.finefindus.eyedropper \
+                      com.github.huluti.Curtail \
+                      org.gnome.Boxes \
+                      org.fedoraproject.MediaWriter \
+                      org.bleachbit.BleachBit \
+                      io.github.thetumultuousunicornofdarkness.cpu-x \
+                      io.github.arunsivaramanneo.GPUViewer \
+                      org.jupyter.JupyterLab \
+                      org.octave.Octave \
+                      com.gitlab.bitseater.meteo \
+                      com.google.Chrome \
+                      org.mozilla.firefox \
+                      com.discordapp.Discord \
+                      com.brave.Browser \
+                      org.telegram.desktop \
+                      com.microsoft.Edge \
+                      org.qbittorrent.qBittorrent \
+                      io.gitlab.librewolf-community \
+                      org.torproject.torbrowser-launcher \
+                      com.opera.Opera \
+                      io.github.ungoogled_software.ungoogled_chromium \
+                      com.vivaldi.Vivaldi \
+                      org.jdownloader.JDownloader \
+                      org.freedownloadmanager.Manager \
+                      nz.mega.MEGAsync \
+                      net.codelogistics.webapps \
+                      io.github.pieterdd.RcloneShuttle \
+                      com.github.Qv2ray \
+                      com.visualstudio.code \
+                      io.github.shiftey.Desktop \
+                      io.podman_desktop.PodmanDesktop \
+                      com.jetpackduba.Gitnuro \
+                      com.gitfiend.GitFiend \
+                      org.jellyfin.JellyfinServer
+
+
+
+
+
+
+
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
